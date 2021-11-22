ae64d996bc1d477f640845c4cd731973b9592037
Samirlc1 <63334911+Samirlc1@users.noreply.github.com>
2021-11-22 16:15:36 +0545

    Add files via upload

---
+++ client.js
@@ -0,0 +1,26 @@
+const socket=io('http://localhost:3000');
+const messages= document.getElementById('messages');
+const msgForm = document.getElementById('msgForm');
+
+socket.on('message', data =>{
+    console.log(data)
+    appendMessages(data)
+})
+socket.on('output-messages',data => {
+    console.log(data)
+    if(data.length){
+        data.forEach(message => {
+            appendMessages(data)
+            });
+    }
+})
+msgForm.addEventListener('submit', e=>{
+e.preventDefault()
+socket.emit('chatmessage',msgForm.msg.value)
+msgForm.msg.value='';
+});
+
+function appendMessages(message) {
+    const html = `<div>${message}</div>`
+    messages.innerHTML += html
+}
