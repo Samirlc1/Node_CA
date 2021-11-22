68e0d623c8f7aca6725a6db4cd81112d32cd10c5
Samirlc1 <63334911+Samirlc1@users.noreply.github.com>
2021-11-22 15:36:53 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+/*
+
+//database connection
+const  Chat  = require("./chatSchema");
+const  connect  = require("./dbconnection");
+
+
+//setup event listener
+io.on("connection", socket  =>  {
+    console.log("user connected");
+    socket.on("disconnect", function() {
+    console.log("user disconnected");
+    });  
+    socket.on("chat message", function(msg) {
+        console.log("message: "  +  msg);
+        //broadcast message to everyone in port:5000 except yourself.
+    socket.broadcast.emit("received", { message: msg  });
+
+    //save chat to the database
+    connect.then(db  =>  {
+    console.log("connected correctly to the server");
+
+    let  chatMessage  =  new Chat({ message: msg, sender: "Anonymous"});
+    chatMessage.save();
+    });
+    });
+});*/
